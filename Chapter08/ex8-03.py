b9263972ee611de15beed23d99418c7aee5b0463
itc-n25012 <n25012@std.it-college.ac.jp>
2025-06-12 02:25:28 +0000

    P118-P122 第8章演習 終了

---
+++ Chapter08/ex8-03.py
@@ -0,0 +1,12 @@
+import statistics
+
+nums = [1, 5, 33, 12, 46, 33, 2]
+
+# 平均値（mean）
+print(statistics.mean(nums))
+
+# 中央値（median）
+print(statistics.median(nums))
+
+# 最頻値（mode）
+print(statistics.mode(nums))
