b46e9c16db9551e25c09a97b00bd3c09f5101bc9
itc-n25012 <n25012@std.it-college.ac.jp>
2025-06-19 02:42:08 +0000

    P139 第10章 チャレンジ問題 終了

---
+++ Chapter10/challenge10-01.py
@@ -0,0 +1,56 @@
+import random
+
+animal_list = ["bird", "cat", "dog", "fish", "rabbit", "snake", "duck", "eagle",
+               "ferret", "cow", "goat", "horse", "pig", "sheep", "falcon", "bear",
+               "deer", "fox", "skunk", "cheetah", "elephant", "lion", "tiger"]
+num = random.randint(0,len(animal_list)-1)
+
+word = animal_list[num]
+
+def hangman(word):
+    wrong = 0
+    stages = ["",
+             "________        ",
+             "|               ",
+             "|        |      ",
+             "|        0      ",
+             "|       /|\\     ",
+             "|       / \\     ",
+             "|               "
+              ]
+
+    rletters = list(word)  # 正解の文字列をリスト化し、変数に代入。
+    board = ["__"] * len(word)
+    win = False
+    print("ハングマンへようこそ")
+    print(" ".join(board)) # joinメソッドを使って、リストの"__"と"__"の間にスペースを追加。
+
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "１文字を予想してね："
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char) # 正解した文字のインデックス番号を変数に代入
+            # 文字列に同じ文字が複数ある場合、当たった文字を伏せ字$にする
+            board[cind] = char 
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+
+        print((" ".join(board)))
+        e = wrong + 1
+        print("\n".join(stages[0: e]))
+
+        # ゲームクリアの処理
+        if "__" not in board:
+            print("あなたの勝ち！")
+            print(" ".join(board))
+            win = True
+            break
+
+    #ゲームオーバーの処理
+    if not win:
+        print("\n".join(stages[0:wrong]))
+        print("あなたの負け！正解は{}.".format(word))
+
+hangman(word)
